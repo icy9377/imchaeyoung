cd82b0f17e3377f135fe016926edcfe908abbc8a
icy9377 <icy93770@gmail.com>
2020-08-10 09:20:37 +0900

    8.10

--- TIL CODE.js
+++ TIL CODE.js
@@ -1,11 +1,4 @@
-//Card 읽는 함수
-if(prefix[0]==='6'){
-    if(cardNumber.length >= 16 || cardNumber.length <= 19){
-      if(prefix.slice(0,2) === '65' || prefix === '6011'){
-        return "Discover"
-    }
-      else if(prefix[1] === '4'){
-        if(prefix[2] === '4' || prefix[2] === '5' || prefix[2] === '6' || prefix[2] === '7' || prefix[2] === '8' || prefix[2] === '9' ){
-          return "Discover"
-      }
-    } 
+// 카드의 첫번째 자리수를 만들자.
+
+      ​}
+      
