3ffff088a9efee74ae03749b6dd2a9f8f571fd6c
icy9377 <icy93770@gmail.com>
2020-10-09 23:01:11 +0900

    friday

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ var app = express();
 var http = require('http').Server(app); //1
 var io = require('socket.io')(http);    //1
 
+
+app.use(express.static('public'));
+app.use(express.static('files'));
+
 app.get('/',function(req, res){  //2
+  //jacascript 파일의 현재 경로에 클라이언트
   res.sendFile(__dirname + '/client.html');
 });
 
-var count=1;
+
+// 유저 idf로 이름 설정하기
 io.on('connection', function(socket){ //3
   console.log('user connected: ', socket.id);  //3-1
-  var name = "user" + count++;                 //3-1
+  var name = `${socket.id}`                 //3-1
   io.to(socket.id).emit('change name',name);   //3-1
 
   socket.on('disconnect', function(){ //3-2
@@ -26,6 +32,6 @@ io.on('connection', function(socket){ //3
   });
 });
 
-http.listen(3000, function(){ //4
+http.listen(8000, function(){ //4
   console.log('server on!');
 });
